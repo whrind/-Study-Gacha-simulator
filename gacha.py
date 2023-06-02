d377743f184c8865baf37c64ed8c42d96de4c2fa
whrind <59040632+whrind@users.noreply.github.com>
2023-06-02 10:36:59 +0800

    Update gacha.py

--- gacha.py
+++ gacha.py
@@ -6,10 +6,10 @@ s3 = [3]    # 40%
 s4 = [4]    # 50%
 s5 = [5]    # 8%
 s6 = [6]    # 2%
-pool = s3*40 + s4*50 + s5*8 + s6*2  #加权重
+pool = s3*400 + s4*500 + s5*80 + s6*20  #加权重
 result = []
 
 for t in range(10):
-    result.append(pool[random.randint(0, 99)])
+    result.append(pool[random.randint(0, 999)])
 
 print('Your result is ', result)
