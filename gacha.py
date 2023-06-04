72acbcd450dd1bc0e8699c543e463ca587fe126e
whrind <59040632+whrind@users.noreply.github.com>
2023-06-05 00:03:09 +0800

    add "gacha" function

--- gacha.py
+++ gacha.py
@@ -1,15 +1,28 @@
 # Gacha? simulator 抽卡模拟器
-# 真随机
+# 真随机 real random
 import random
 
-s3 = [3]    # 40%
-s4 = [4]    # 50%
-s5 = [5]    # 8%
-s6 = [6]    # 2%
-pool = s3*400 + s4*500 + s5*80 + s6*20  #加权重
-result = []
 
-for t in range(10):
-    result.append(pool[random.randint(0, 999)])
+def main():
+    s3 = [3]    # 40%
+    s4 = [4]    # 50%
+    s5 = [5]    # 8%
+    s6 = [6]    # 2%
+    pool = s3*400 + s4*500 + s5*80 + s6*20  #加权重 weight 1000
 
-print('Your result is ', result)
+
+def onetime(pool):
+    print('Your result is ', pool[random.randint(0, 999)])
+
+
+def tentime(pool):
+    result = []
+    for _ in range(10):
+        result.append(pool[random.randint(0, 999)])
+    print('Your result is ', result)
+
+def gacha():
+
+
+
+main()
